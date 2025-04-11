e42e6f6cf0cc36669a9ce0ec9347c139209c2839
gunyu1019 <gunyu1019@gmail.com>
2025-04-11 08:20:23 +0000

    [Feat] Add control segmented button component.

---
+++ lib/components/control/segmented_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+abstract class SegmentedButtonItem {
+  abstract String text;
+}
+
+class ControlSegmentedButton<T> extends StatefulWidget {
+  const ControlSegmentedButton(
+      {super.key,
+      required this.title,
+      required this.values,
+      required this.onChanged,
+      this.titleTextStyle,
+      this.valueTextStyle,
+      this.initialValue});
+
+  final String title;
+  final TextStyle? titleTextStyle;
+  final TextStyle? valueTextStyle;
+
+  final List<T> values;
+  final T? initialValue;
+  final void Function(T) onChanged;
+
+  @override
+  State<StatefulWidget> createState() => _ControlSegmentedButtonState();
+}
+
+class _ControlSegmentedButtonState<T extends SegmentedButtonItem> extends State<ControlSegmentedButton> {
+  late T selectedButton;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      selectedButton = widget.initialValue ?? widget.values[0];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(widget.title,
+              style: widget.titleTextStyle, textAlign: TextAlign.start),
+        SegmentedButton<T>(
+          segments: widget.values.map((element) => ButtonSegment<T>(
+              value: element,
+              label: Text(element.text),
+            )).toList(),
+          selected: <T>{selectedButton},
+          onSelectionChanged: (Set<T> newSelection) {
+            setState(() {
+              // By default there is only a single segment that can be
+              // selected at one time, so its value is always the first
+              // item in the selected set.
+              selectedButton = newSelection.first;
+            });
+          },
+        )
+      ]);
+}
