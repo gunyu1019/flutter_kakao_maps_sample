2e352dee8352c326c0858943f350f66f4696e714
gunyu1019 <gunyu1019@gmail.com>
2025-04-16 11:23:52 +0000

    [Feat] Add navigation menu at Route enumeration

--- lib/enums/routes.dart
+++ lib/enums/routes.dart
@@ -3,12 +3,14 @@ import 'package:kakao_map_sdk_example/models/menu_info.dart';
 import 'package:kakao_map_sdk_example/pages/menu/camera_option_menu.dart';
 import 'package:kakao_map_sdk_example/pages/menu/home_menu.dart';
 import 'package:kakao_map_sdk_example/pages/menu/map_setting_menu.dart';
+import 'package:kakao_map_sdk_example/pages/menu/navigation_menu.dart';
 // import 'package:kakao_map_sdk_example/pages/menu/poi_overlay_menu.dart';
 
 enum Routes {
   homeMenu(HomeMenu.menuInfo, HomeMenu()),
   cameraOptionMenu(CameraOptionMenu.menuInfo, CameraOptionMenu()),
   // poiOvarlayMenu(PoiOverlayMenu.menuInfo, PoiOverlayMenu()),
+  navigationMenu(NavigationMenu.menuInfo, NavigationMenu()),
   mapSettingMenu(MapSettingMenu.menuInfo, MapSettingMenu());
 
   final MenuInfo menuInfo;
