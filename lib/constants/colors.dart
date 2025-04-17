65ba5518a9b31617a2e54b58762920a3ea5d12be
gunyu1019 <gunyu1019@gmail.com>
2025-04-17 15:01:48 +0000

    [Feat] Add constant data (colors, textstyles)

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+extension AppColors on Colors {
+  static const Color textColor = Colors.black;
+  
+  static const Color flutterColor = Color.fromARGB(255, 19, 137, 253);
+  static const Color androidColor = Color.fromARGB(255, 50, 222, 132);
+  static const Color iOSColor = Color.fromARGB(255, 0, 0, 0);
+}
---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_map_sdk_example/constants/colors.dart';
+
+final class TextStyles {
+  static const titleTextStyle = TextStyle(
+      fontSize: 24,
+      color: AppColors.textColor,
+      decoration: TextDecoration.none,
+      fontWeight: FontWeight.bold);
+    
+}
